d2f4daefd659a6f85f976efb6379a4643149185a
Tushar Sharma <tsharma@HW15645.local>
2019-02-06 14:59:33 +0530

    first commit

---
+++ modifyProps.py
@@ -0,0 +1,38 @@
+import sys
+import shlex
+import json
+
+class modProp:
+	def __init__(self):
+		with open('params.json') as runSet:
+			self.params=json.load(runSet)
+
+	def modifyProperties(self,filepath,propDict):
+		try:
+			f=open(filepath,'r+')
+			existingProps=[line.strip(' \n') for line in f.readlines()]
+			f.close()
+			w=open(filepath,'w+')
+			for prop in existingProps:
+				try:
+					if not prop.strip(' \n'):
+						w.write('\n')
+						continue
+					if prop.startswith('#') or not len(shlex.split(prop))==2:
+						w.write(prop+'\n')
+						continue
+					key,val=shlex.split(prop)
+					if key in propDict.keys():
+						val=propDict[key]
+					w.write('{}    {}'.format(key,val)+'\n')
+				except Exception as e:
+					print e.__str__()
+			w.close()
+		except Exception as e:
+			print e.__str__()
+
+	def configure(self):
+		for conf in self.params['wrap']:
+			self.modifyProperties(conf['filepath'],conf['properties'])
+
+
---
+++ runMLBench.py
@@ -0,0 +1,41 @@
+import sys
+import subprocess
+import modifyProps
+import logging
+
+class triggerMlBench:
+
+	def __init__(self):
+		modifyProps.modProp().configure()
+		FORMAT = '%(asctime)-s-%(levelname)s-%(message)s'
+		logging.basicConfig(format=FORMAT,filename='mlbench.log',filemode='w',level='INFO')
+		logging.getLogger("requests").setLevel(logging.WARNING)
+		self.logger=logging.getLogger(__name__)
+		if len(sys.argv)==1:
+			self.tests=["lr","als","svm","linear","kmeans"]
+		else:
+			self.tests=sys.argv[1].split(',')
+
+	def runShell(self,cmd):
+		try:
+			self.logger.info("+Running command {}".format(cmd))
+			result=subprocess.check_output(cmd,stderr=subprocess.STDOUT,shell=True)
+			self.logger.debubg(result)
+			self.logger.info("-Finished running command {}".format(cmd))
+		except Exception as e:
+			self.logger.warn("-Error while running command {}".format(cmd))
+			self.logger.warn(e.__str__())
+
+	def benchmark(self):
+		self.logger.info('**** Running tests ****')
+		self.runShell('truncate -s 0 report/hibench.report')
+		for test in self.tests:
+			self.logger.info("+Running {} test".format(test))	
+			self.runShell('bin/workloads/ml/{}/prepare/prepare.sh'.format(test))
+			self.runShell('bin/workloads/ml/{}/spark/run.sh'.format(test))
+			self.logger.info("-Finished running {} test".format(test))	
+		self.logger.info('**** Finished tests ****')
+		self.logger.info('report/hibench.report','r+').read()
+
+if __name__=='__main__':
+	triggerMlBench().benchmark()
